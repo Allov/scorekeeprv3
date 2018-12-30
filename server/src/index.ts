9e2a47bf8894b781388b593b844391367c1f1f48
Fynnen <lapierre.francis@gmail.com>
2018-12-30 18:18:41 -0500

    initial commit of graphqlserver

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express = require('express');
+import { ApolloServer, gql } from 'apollo-server-express';
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+const port = 4000;
+
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
