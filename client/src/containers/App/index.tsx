b01e751788d5d0ce1849ceb40defa9de6f5b9f4f
Allov <frederik.charest@gmail.com>
2019-01-05 14:25:06 -0500

    connect theme

--- client/src/containers/App/index.tsx
+++ client/src/containers/App/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import Layout from '../../components/Layout';
 import Home from '../Home';
+import { makeSelectTheme } from './selectors';
 
 interface IAppProps {
-  className?: string,
+  className?: string;
+  theme: string;
 }
 
 export const App = (props: IAppProps) => {
@@ -20,4 +24,8 @@ export const App = (props: IAppProps) => {
   );
 };
 
-export default App;
+const mapStateToProps = () => createStructuredSelector({
+  theme: makeSelectTheme(),
+});
+
+export default connect(mapStateToProps)(App);
--- client/src/containers/App/tests/index.test.tsx
+++ client/src/containers/App/tests/index.test.tsx
@@ -1,9 +1,9 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from '..';
+import { App } from '..';
 
 it('renders <App /> container without crashing', () => {
-  const renderedComponent = shallow(<App />);
+  const renderedComponent = shallow(<App theme="DEFAULT" />);
   expect(renderedComponent.find(Router).length).toEqual(1);
 });
