90dc10b2a3e40ef015822265198d716e6f21a39b
Fynnen <lapierre.francis@gmail.com>
2019-01-15 21:37:24 -0500

    add resolvers for round.players and player.totalscore

--- server/src/infrastructure/scores/score.resolvers.ts
+++ server/src/infrastructure/scores/score.resolvers.ts
@@ -1,5 +1,7 @@
+import { IPlayer } from 'infrastructure/players/player.types';
+import { Types } from 'mongoose';
 import { Game as GameRepository } from '../games/game.model';
-import { IScoreFilterInput, IScoresInput } from './score.types';
+import { IScore, IScoreFilterInput, IScoresInput } from './score.types';
 
 export async function addScoresToRound(_: any, { input }: { input: IScoresInput }) {
 
@@ -48,10 +50,19 @@ export async function updateScore(_: any, { id, input }: { id: any, input: IScor
   return game;
 }
 
+export async function player(score: IScore) {
+  const playerId = new Types.ObjectId(score.playerId);
+  const game = await GameRepository.findOne({'players._id': playerId});
+  return game.players.find(p => p.id.toString() === score.playerId);
+}
+
 export const scoreResolvers = {
   Mutation: {
     addScoresToRound,
     deleteScore,
     updateScore,
-  }
+  },
+  Score: {
+    player,
+  },
 };
